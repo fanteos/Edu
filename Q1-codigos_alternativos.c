c672a3bfa7c8b8446a0b15883d272b9b14099619
fanteos <fante.antunes@outlook.com>
2018-03-19 18:06:05 -0300

    Q1 - códigos alternativos
    Os códigos alternativos da questão 01

---
+++ Q1-codigos_alternativos.c
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+float numero_1;
+"%f"
+
+
+
+scanf("%d", &numero_1);
+scanf("%d", &numero_2);
+scanf("%d", &numero_3);
+
+
+if((numero_1 >= numero_2) && (numero_1 >= numero_3))
+    printf("\nO maior numero eh: %d", numero_1);
+if((numero_2 >= numero_1) && (numero_2 >= numero_3))
+    printf("\nO maior numero eh: %d", numero_2);
+if((numero_3 >= numero_1) && (numero_3 >= numero_2))
+    printf("\nO maior numero eh: %d", numero_3);
